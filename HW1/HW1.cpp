24d4976dd14b1aef0e3d5d076a3ee0718ba9e9e4
Huynhkimphat <hkimphat2012@gmail.com>
2020-02-03 16:29:15 +0700

    Update HW1.cpp

--- HW1/HW1.cpp
+++ HW1/HW1.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
-using namespace std;
-int main()
-{
+#include <iostream>
+#include <ctime>
 
+using namespace std;
+
+int main() {
+    // current date/time based on current system
+    time_t now = time(0);
+
+    // convert now to string form
+    char* dt = ctime(&now);
+
+    cout << "The local date and time is: " << dt << endl;
 }
