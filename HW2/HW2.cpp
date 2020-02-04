67cfa307a31fe00c843245a593638713b0c9b443
Huynhkimphat <hkimphat2012@gmail.com>
2020-02-04 16:12:29 +0700

    Update HW2.cpp

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -84,7 +84,22 @@ void HW2c()
 }
 void HW2d()
 {
-    
+    int amount = 0;
+    for (int i = 1; i <= 4; i++)
+    {
+        for (int j = 1; j <= 4; j++)
+        {
+            for (int k = 1; k <= 4; k++)
+            {
+                if (k != i && k != j && i != j)
+                {
+                    amount++;
+                    cout << i << j << k << " ";
+                }
+            }
+        }
+    }
+    cout << endl << " Total: " << amount << endl << endl;
 }
 void HW2e()
 {
