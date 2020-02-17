509092373885c0fd3a31ea7dc0f773f7145fb81b
Huynhkimphat <hkimphat2012@gmail.com>
2020-02-17 08:57:00 +0700

    Create HW1b.cpp

---
+++ HW1b/HW1b.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include <stdlib.h> 
+using namespace std;
+int main()
+{
+    char a[256];
+    cin >> a;
+    int k = strlen(a);
+    int x = 0, y = 0, z = 0, w = 0;
+    for (int i = 0; i < strlen(a); i++)
+    {
+        if (a[i] >= '1' && a[i] <= '9')
+        {
+            int t = a[i] - '0';
+            y = y * 10 + t;
+        }
+        if (w == 0)
+        {
+            x = y; w = 1; y = 0;
+        }
+        if (a[i] == '+')
+        {
+            if (z == -1 || z==1)
+            {
+                x = x + y * z;
+                y = 0;
+            }
+            z = 1;
+        }
+        if (a[i] == '-')
+        {
+            if (z == -1 || z == 1)
+            {
+                x = x + y * z;
+                y = 0;
+            }
+            z = -1;
+        }
+
+    }
+    x = x + y * z;
+    cout << x;
+    return 0;
+}
